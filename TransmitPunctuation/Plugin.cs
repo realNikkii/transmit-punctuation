68eafcb3c7a886332548bab883ebff06a7b4d67d
Nikki <nikecoes@gmail.com>
2024-03-18 11:28:49 +0100

    Initial commit

---
+++ TransmitPunctuation/Plugin.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace TransmitPunctuation
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private readonly Harmony _harmony = new(PluginInfo.PLUGIN_GUID);
+        
+        private void Awake()
+        {
+            _harmony.PatchAll(typeof(TerminalPatch));
+        }
+    }
+}
---
+++ TransmitPunctuation/TerminalPatch.cs
@@ -0,0 +1,15 @@
+using HarmonyLib;
+
+namespace TransmitPunctuation;
+
+[HarmonyPatch(typeof(Terminal))]
+public class TerminalPatch
+{
+    [HarmonyPatch("RemovePunctuation")]
+    [HarmonyPrefix]
+    private static bool RemovePunctuation(object[] __args, ref string __result)
+    {
+        __result = __args[0].ToString();
+        return false;
+    }
+}
